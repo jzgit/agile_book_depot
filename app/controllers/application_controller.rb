fe41158d375a1f8c271165d0d6139c0eba1c3ec6
Jason Zopf <jasonzopf@gmail.com>
2011-04-15 18:29:25 -0400

    chap 14

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_filter :authorize 
+
   protect_from_forgery
 
   private
@@ -11,5 +13,11 @@ class ApplicationController < ActionController::Base
       cart
     end
 
+  protected
+    def authorize
+      unless User.find_by_id(session[:user_id])
+        redirect_to login_url, :notice => "Please log in"
+      end
+    end
 
 end
